e7373ae61eb4b323605672fbb446282ac1f5acb5
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-07-11 21:23:19 -0300

    alterando_a_visibidade_do_atributo_saldo_para_'private'_para_evitar_alteracao_do_mesmo_fora_da_classe

--- POO/encapsulamento.php
+++ POO/encapsulamento.php
@@ -2,27 +2,32 @@
 
 class Conta {       // classe 'Conta'
     
-    public int $saldo;      // 'atributo' $saldo
+    private int $saldo;      // definindo atributo como 'private' para evitar alteração fora da classe
 
-    public function __contruct(int $saldo) {        // método que define o 'saldo inicial' da conta
-        $this-> saldo = $saldo;     // atribuindo 
+    public function __contruct(int $saldo) {
+        $this-> saldo = $saldo;
     }
 
-    public function sacar(int $valor) {     // método 'sacar'
+    public function sacar(int $valor) {
         echo 'Voçê sacou: '. $valor. '<br>';
         $this-> saldo -= $valor;
     }
 
-    public function depositar(int $valor) {     // método 'depositar'
+    public function depositar(int $valor) {
         echo 'Voçê depositou: '. $valor. '<br>';
         $this-> saldo += $valor;
     }
+
+    public function verSaldo() {        // função 'pública' verSaldo retorna saldo 'private' de dentro da classe
+        return $this-> saldo;
+    }
 }
 
-$conta1 = new Conta(500);       // criando 'conta1'
+$conta1 = new Conta(500);
 
-$conta1-> depositar(500);   // depositando
-echo 'Saldo Inicial: '. $conta1-> saldo. '<br>';
-echo $conta1-> saldo;
+$conta1-> depositar(500);
+echo 'Saldo Inicial: '. $conta1-> verSaldo(). '<br>';
+$conta1-> depositar(100);
+$conta1-> sacar(500);
 
-echo 'Saldo Atual: '. $conta1-> saldo. '<br>';
+echo 'Saldo Atual : '. $conta1-> verSaldo(). '<br>';
